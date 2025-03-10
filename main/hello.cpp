3fbde7f636318dc451cdf87a0b445f8ffbdd1f73
aakankshseelin <iamakshus@gmail.com>
2025-03-10 16:25:29 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "My srn is PES2UG22CS003 and my name is Aakanksh ";
+  cout << "My srn is PES2UG22CS003 and my name is Aakanksh ,";
   return 0;
 }
